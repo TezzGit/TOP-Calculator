c824771576af950872eed8a37256779965db1d69
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-29 12:42:46 +1100

    Assign Buttons to JS Vars

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Numpad Digits
+const btnNum1 = document.getElementById('np-1');
+const btnNum2 = document.getElementById('np-2');
+const btnNum3 = document.getElementById('np-3');
+const btnNum4 = document.getElementById('np-4');
+const btnNum5 = document.getElementById('np-5');
+const btnNum6 = document.getElementById('np-6');
+const btnNum7 = document.getElementById('np-7');
+const btnNum8 = document.getElementById('np-8');
+const btnNum9 = document.getElementById('np-9');
+const btnNum0 = document.getElementById('np-0');
+const btnDecimal = document.getElementById('np-decimal')
+
+// Operator Buttons
+const btnMultiply = document.getElementById('multiply');
+const btnDivide = document.getElementById('divide');
+const btnSum = document.getElementById('sum');
+const btnSubtract = document.getElementById('subtract');
+const btnEqual = document.getElementById('equal');
+
+// Function Buttons
+const btnPercent = document.getElementById('np-percent');
+const btnPlusMinus = document.getElementById('np-plus-minus')
+const btnBackspace = document.getElementById('bksp');
+const btnClear = document.getElementById('clear');
+
+
